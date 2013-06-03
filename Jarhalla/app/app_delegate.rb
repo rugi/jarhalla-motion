316d11438f31ad00abaabd67f0b3acaebfea66fe
Isaac Ruiz Guerra <isaac.ruiz.guerra@tidyslice.com>
2013-06-02 20:59:33 -0500

    prueba de layout

--- Jarhalla/app/app_delegate.rb
+++ Jarhalla/app/app_delegate.rb
@@ -11,5 +11,18 @@ class AppDelegate
       defer: false)
     @mainWindow.title = NSBundle.mainBundle.infoDictionary['CFBundleName']
     @mainWindow.orderFrontRegardless
+
+    @text_search = NSTextField.alloc.initWithFrame(NSMakeRect(20, 318, 341, 22))
+    @text_search.stringValue = "xcode crash"
+    @text_search.autoresizingMask = NSViewMinXMargin|NSViewMinYMargin|NSViewWidthSizable
+    @mainWindow.contentView.addSubview(@text_search)
+
+    button = NSButton.alloc.initWithFrame(NSMakeRect(384, 310, 82, 32))
+    button.title = "Search"
+    #button.action = :"search:"
+    button.target = self
+    button.bezelStyle = NSRoundedBezelStyle
+    button.autoresizingMask = NSViewMinXMargin|NSViewMinYMargin
+    @mainWindow.contentView.addSubview(button)    
   end
 end
---
+++ Jarhalla/build/MacOSX-10.8-Development/objs/init.mm
@@ -0,0 +1,43 @@
+extern "C" {
+    void ruby_sysinit(int *, char ***);
+    void ruby_init(void);
+    void ruby_init_loadpath(void);
+    void ruby_script(const char *);
+    void ruby_set_argv(int, char **);
+    void rb_vm_init_compiler(void);
+    void rb_vm_init_jit(void);
+    void rb_vm_aot_feature_provide(const char *, void *);
+    void *rb_vm_top_self(void);
+    void rb_rb2oc_exc_handler(void);
+    void rb_exit(int);
+void MREP_B5ADF801C7FD40EEA18168186FB523DB(void *, void *);
+void MREP_D7480A0DB99747D5AD3575572058BB15(void *, void *);
+}
+
+extern "C"
+void
+RubyMotionInit(int argc, char **argv)
+{
+    static bool initialized = false;
+    if (!initialized) {
+	ruby_init();
+	ruby_init_loadpath();
+        if (argc > 0) {
+	    const char *progname = argv[0];
+	    ruby_script(progname);
+	}
+#if !__LP64__
+	try {
+#endif
+	    void *self = rb_vm_top_self();
+MREP_B5ADF801C7FD40EEA18168186FB523DB(self, 0);
+MREP_D7480A0DB99747D5AD3575572058BB15(self, 0);
+#if !__LP64__
+	}
+	catch (...) {
+	    rb_rb2oc_exc_handler();
+	}
+#endif
+	initialized = true;
+    }
+}
